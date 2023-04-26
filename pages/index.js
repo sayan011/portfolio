050885480c60219aee51a3f3255e5929e6782e94
sayan <98570396+sayan011@users.noreply.github.com>
2023-04-26 19:11:02 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ import styles from "../styles/Home.module.css";
 export default function Home() {
   return (
     <div>
-      <div className="grid h-screen place-items-center">wOrK iN pRogrEsS..<br/>now go away...shoo</div>
+      <div className="grid h-screen place-items-center">wOrK iN pRogrEsS</div>
       
       <Image src="/cat.jpg" alt="cattt" width={90} height={22} />
      
