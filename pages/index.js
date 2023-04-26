fb31ecc303982570a553307ba941876f83099631
sayan <98570396+sayan011@users.noreply.github.com>
2023-04-26 19:12:47 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,8 +6,7 @@ export default function Home() {
   return (
     <div>
       <div className="grid h-screen place-items-center">wOrK iN pRogrEsS</div>
-      
-      <Image src="/cat.jpg" alt="cattt" width={90} height={22} />
+     
      
     </div>
   );
